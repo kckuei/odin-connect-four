d121381347c513ca9da448b26531420c2163cdb7
kckuei <kevinkuei@gmail.com>
2022-09-25 19:57:09 -0700

    Add init methods for ConnectFour and Player classes

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -3,6 +3,11 @@
 require_relative './board'
 require_relative './player'
 
+# Connect Four class for playing a connect4 game
 class ConnectFour
-  def make_gameboard; end
+  def make_gameboard(rows = 6, columns = 7)
+    @board = Board.new(rows, columns)
+    @player1 = nil
+    @player2 = nil
+  end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
+# A player class
 class Player
+  def initialize(name)
+    @name = name
+    @wins = 0
+    @losses = 0
+  end
 end
